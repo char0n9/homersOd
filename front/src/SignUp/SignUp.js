44a69d7d4ea4307c27831a39bc68c7616c3f1ad5
char0n9 <ionascu.emil@gmail.com>
2020-06-22 11:55:58 +0300

    quest 020

--- front/src/SignUp/SignUp.js
+++ front/src/SignUp/SignUp.js
@@ -5,18 +5,53 @@ export default class SignUp extends React.Component {
         super(props)
     
         this.state = {
-             email: '',
+            email: "mon@email.com",
+            password: "monPassw0rd",
+            passwordVerification:'',
+            firstName: "James",
+            lastName: "Bond",
         }
         this.updateEmailField = this.updateEmailField.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     updateEmailField = (event) => {this.setState({email: event.target.value})
 
     } 
+
+    updatePasswordField = (event) => {
+        this.setState({password: event.target.value})
+    }
+    updateLastNameField = (event) => {
+        this.setState({lastName: event.target.value})
+    }
+    
+    updateFirstNameField = (event) => {
+        this.setState({firstName: event.target.value})
+    }
+    updatePasswordVerifictionField = (event) => {
+        this.setState({passwordVerification: event.target.value})
+    }
+
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(this.state)
+    }
   render() {
     return(<div>
-        <h1>{this.state.email}</h1> 
-     <input type="email" name="email" onChange={this.updateEmailField}   /> 
+        <h1>{JSON.stringify(this.state,1,1)}</h1> 
+        <form>
+     <input type="email" name="email" onChange={this.updateEmailField}   />
+     <input type="password" name="password" onChange={this.updatePasswordField}   />
+     <input type="password" name="passworddbis" onChange={this.updatePasswordVerifictionField}   />
+     <input type="text" name="lastname" onChange={this.updateLastNameField}   />
+     <input type="text" name="firstname" onChange={this.updateFirstNameField}   />
+  
+    
+     
+     
+     <input type="submit" value="Submit" onClick ={this.handleSubmit}/>
+      </form>
     
     </div>);
   }
